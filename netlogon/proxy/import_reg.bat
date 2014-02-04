0aa165030e2a60f87c7361ede4f073ccc77fff3e
Jeferson Lopes <jeferson.lopes@live.com>
2014-02-04 10:55:35 -0200

    Set Proxy with netlogon
    Configurações de Proxy para o S.O. através de script .bat no logon do
    usuário;

--- netlogon/proxy/import_reg.bat
+++ netlogon/proxy/import_reg.bat
@@ -1,19 +1,38 @@
 REM FOR /F "TOKENS=2* DELIMS=:" %%a in ('IPCONFIG ^| find "IPv4" ^| find "10.0."') do for %%b in (%%a) Do set localIp=%%b
+REM Autor: Jeferson Lopes @2014
+FOR /F "tokens=1 delims=:" %%d IN ('ping %computername% -4 -n 1 ^| find /i "resposta"') DO set lip=%%d
+set lip=%lip:~12,8%
+set pon=10.0.18.
+set po1=10.0.16.
+set po2=10.0.15.
+set po3=10.0.14.
 
-FOR /F "tokens=1 delims=:" %%d IN ('ping %computername% -4 -n 1 ^| find /i "resposta"') DO set localIp=%%d
-
-set localIp=%localIp:~12,8%
-
-if "%localIp" equ "10.0.18." (
-
-rem REDE COM PROXY
-
-regedit /s \\aplicacao\NETLOGON\browser\proxy_on.reg
-
+if %lip%==%pon% (
+	C:\Windows\regedit.exe /s \\aplicacao\NETLOGON\browser\proxy_on.reg
+	GOTO :FIM
 ) ELSE (
-
-rem REDE EXCECAO DE PROXY
-
-regedit /s \\aplicacao\NETLOGON\browser\proxy_off.reg
-
+GOTO :REDE1
 )
+:REDE1
+if %lip%==%po1% (
+	C:\Windows\regedit.exe /s \\aplicacao\NETLOGON\browser\proxy_off.reg
+	GOTO :FIM
+) ELSE (
+GOTO :REDE2
+)
+:REDE2
+if %lip%==%po2% (
+	C:\Windows\regedit.exe /s \\aplicacao\NETLOGON\browser\proxy_on.reg
+	GOTO :FIM
+) ELSE (
+GOTO :REDE3
+)
+:REDE3
+if %lip%==%po3% (
+	C:\Windows\regedit.exe /s \\aplicacao\NETLOGON\browser\proxy_on.reg
+	GOTO :FIM
+) ELSE (
+	C:\Windows\regedit.exe /s \\aplicacao\NETLOGON\browser\proxy_on.reg
+	GOTO :FIM
+)
+:FIM
